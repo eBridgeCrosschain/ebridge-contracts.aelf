be2cb09d35cd137cd725a378d8272d6b039cab0b
sherry <sherry@ebridge.exchange>
2023-11-07 17:02:10 +0800

    feat：add unit test.

--- contract/EBridge.Contracts.Bridge/BridgeContract_Limit.cs
+++ contract/EBridge.Contracts.Bridge/BridgeContract_Limit.cs
@@ -1,3 +1,4 @@
+using System;
 using AElf.CSharp.Core;
 using AElf.Sdk.CSharp;
 using AElf.Types;
@@ -16,13 +17,17 @@ public partial class BridgeContract
         foreach (var dailyReceiptLimitInfo in input.DailyReceiptLimitInfos)
         {
             Assert(
-                dailyReceiptLimitInfo.DefaultTokenAmount > 0 && dailyReceiptLimitInfo.Chain != null &&
-                dailyReceiptLimitInfo.Symbol != null &&
-                dailyReceiptLimitInfo.StartTime.Seconds % DefaultDailyRefreshTime == 0, "Invalid input daily receipt limit info.");
+                dailyReceiptLimitInfo.DefaultTokenAmount > 0 &&
+                !string.IsNullOrEmpty(dailyReceiptLimitInfo.TargetChain) &&
+                !string.IsNullOrEmpty(dailyReceiptLimitInfo.Symbol) &&
+                dailyReceiptLimitInfo.StartTime.Seconds % DefaultDailyRefreshTime == 0,
+                "Invalid input daily receipt limit info.");
             var symbol = dailyReceiptLimitInfo.Symbol;
-            var targetChain = dailyReceiptLimitInfo.Chain;
+            var targetChain = dailyReceiptLimitInfo.TargetChain;
             var dailyLimit = State.DailyReceiptLimit[symbol][targetChain] ?? new DailyLimitTokenInfo();
-            dailyLimit = SetDailyLimit(dailyLimit, dailyReceiptLimitInfo.DefaultTokenAmount, dailyReceiptLimitInfo.StartTime);
+            dailyLimit = SetDailyLimit(dailyLimit, dailyReceiptLimitInfo.DefaultTokenAmount,
+                dailyReceiptLimitInfo.StartTime);
+            State.DailyReceiptLimit[symbol][targetChain] = dailyLimit;
             Context.Fire(new DailyReceiptLimitSet
             {
                 Symbol = symbol,
@@ -43,12 +48,13 @@ public partial class BridgeContract
         {
             Assert(
                 dailySwapLimitInfo.DefaultTokenAmount > 0 && dailySwapLimitInfo.SwapId != null &&
-                dailySwapLimitInfo.StartTime.Seconds % DefaultDailyRefreshTime == 0, "Invalid input daily swap limit info.");
+                dailySwapLimitInfo.StartTime.Seconds % DefaultDailyRefreshTime == 0,
+                "Invalid input daily swap limit info.");
             var swapInfo = State.SwapInfo[dailySwapLimitInfo.SwapId];
-            Assert(swapInfo != null,"Invalid swap id.");
+            Assert(swapInfo != null, "Invalid swap id.");
             var dailyLimit = State.DailySwapLimit[dailySwapLimitInfo.SwapId] ?? new DailyLimitTokenInfo();
             dailyLimit = SetDailyLimit(dailyLimit, dailySwapLimitInfo.DefaultTokenAmount, dailySwapLimitInfo.StartTime);
-            
+            State.DailySwapLimit[dailySwapLimitInfo.SwapId] = dailyLimit;
             Context.Fire(new DailySwapLimitSet
             {
                 Symbol = swapInfo?.SwapTargetToken?.Symbol,
@@ -61,22 +67,50 @@ public partial class BridgeContract
         return new Empty();
     }
 
-    private DailyLimitTokenInfo SetDailyLimit(DailyLimitTokenInfo dailyLimit, long defaultTokenAmount, Timestamp startTime)
+    private DailyLimitTokenInfo SetDailyLimit(DailyLimitTokenInfo dailyLimit, long defaultTokenAmount,
+        Timestamp startTime)
     {
+        var defaultRefreshTime = State.DailyLimitRefreshTime.Value == 0
+            ? DefaultDailyRefreshTime
+            : State.DailyLimitRefreshTime.Value;
+        if (dailyLimit.RefreshTime != null &&
+            (dailyLimit.RefreshTime - Context.CurrentBlockTime).Seconds.Div(defaultRefreshTime) <= 0)
+        {
+            var useAmount = dailyLimit.DefaultTokenAmount.Sub(dailyLimit.TokenAmount);
+            dailyLimit.TokenAmount = defaultTokenAmount.Sub(useAmount) < 0 ? 0 : defaultTokenAmount.Sub(useAmount);
+        }
+        else
+        {
+            dailyLimit.TokenAmount = defaultTokenAmount;
+        }
+
         dailyLimit.DefaultTokenAmount = defaultTokenAmount;
-        dailyLimit.TokenAmount = defaultTokenAmount;
         dailyLimit.RefreshTime = startTime;
         return dailyLimit;
     }
 
     public override DailyLimitTokenInfo GetDailyReceiptLimit(GetDailyReceiptLimitInput input)
     {
-        return State.DailyReceiptLimit[input.Symbol][input.Chain] ?? new DailyLimitTokenInfo();
+        return State.DailyReceiptLimit[input.Symbol][input.TargetChain] ?? new DailyLimitTokenInfo();
     }
 
     public override DailyLimitTokenInfo GetDailySwapLimit(Hash input)
     {
         return State.DailySwapLimit[input] ?? new DailyLimitTokenInfo();
+    }
+
+    public override Empty SetDailyLimitRefreshTime(Int64Value input)
+    {
+        State.DailyLimitRefreshTime.Value = input.Value;
+        return new Empty();
+    }
+
+    public override Int64Value GetDailyLimitRefreshTime(Empty input)
+    {
+        return new Int64Value
+        {
+            Value = State.DailyLimitRefreshTime.Value
+        };
     }
 
     #endregion
@@ -85,51 +119,76 @@ public partial class BridgeContract
 
     public override Empty ConfigReceiptTokenBucket(ConfigReceiptTokenBucketInput input)
     {
-        Assert(Context.Sender == State.Admin.Value,"No permission.");
-        Assert(input.ReceiptTokenBucketConfigs.Count > 0 , "Invalid input");
+        Assert(Context.Sender == State.Admin.Value, "No permission.");
+        Assert(input.ReceiptTokenBucketConfigs.Count > 0, "Invalid input.");
         foreach (var config in input.ReceiptTokenBucketConfigs)
         {
-            Assert(config.Chain != null && config.Symbol != null && config.TokenCapacity > 0 && config.Rate > 0,"Invalid receipt bucket config input.");
-            var bucketConfig = State.ReceiptTokenBucketInfo[config.Symbol][config.Chain] ?? new TokenBucket();
+            Assert(
+                !string.IsNullOrEmpty(config.TargetChain) && !string.IsNullOrEmpty(config.Symbol) &&
+                config.TokenCapacity > 0 && config.Rate > 0,
+                "Invalid receipt bucket config input.");
+            var bucketConfig = State.ReceiptTokenBucketInfo[config.Symbol][config.TargetChain] ?? new TokenBucket();
             bucketConfig = SetTokenBucketConfig(bucketConfig, config.TokenCapacity, config.Rate, config.IsEnable);
+            State.ReceiptTokenBucketInfo[config.Symbol][config.TargetChain] = bucketConfig;
             Context.Fire(new ReceiptTokenBucketSet
             {
                 Symbol = config.Symbol,
-                TargetChainId = config.Chain,
+                TargetChainId = config.TargetChain,
                 ReceiptBucketIsEnable = bucketConfig.IsEnable,
                 ReceiptCapacity = bucketConfig.TokenCapacity,
-                ReceiptRefillRate = bucketConfig.Rate
+                ReceiptRefillRate = bucketConfig.Rate,
+                ReceiptBucketUpdateTime = Context.CurrentBlockTime
             });
         }
+
         return new Empty();
     }
-    
+
     public override Empty ConfigSwapTokenBucket(ConfigSwapTokenBucketInput input)
     {
-        Assert(Context.Sender == State.Admin.Value,"No permission.");
-        Assert(input.SwapTokenBucketConfigs.Count > 0 , "Invalid input");
+        Assert(Context.Sender == State.Admin.Value, "No permission.");
+        Assert(input.SwapTokenBucketConfigs.Count > 0, "Invalid input.");
         foreach (var config in input.SwapTokenBucketConfigs)
         {
-            var swapInfo = State.SwapInfo[config.SwapId];
-            Assert(swapInfo != null && config.TokenCapacity > 0 && config.Rate > 0,"Invalid swap bucket config input.");
+            Assert(config.SwapId != null && config.TokenCapacity > 0 && config.Rate > 0,
+                "Invalid swap bucket config input.");
+            var swapInfo = GetTokenSwapInfo(config.SwapId);
             var bucketConfig = State.SwapTokenBucketInfo[config.SwapId] ?? new TokenBucket();
             bucketConfig = SetTokenBucketConfig(bucketConfig, config.TokenCapacity, config.Rate, config.IsEnable);
+            State.SwapTokenBucketInfo[config.SwapId] = bucketConfig;
             Context.Fire(new SwapTokenBucketSet
             {
                 Symbol = swapInfo?.SwapTargetToken?.Symbol,
                 FromChainId = swapInfo?.SwapTargetToken?.FromChainId,
                 SwapBucketIsEnable = bucketConfig.IsEnable,
                 SwapCapacity = bucketConfig.TokenCapacity,
-                SwapRefillRate = bucketConfig.Rate
+                SwapRefillRate = bucketConfig.Rate,
+                SwapBucketUpdateTime = Context.CurrentBlockTime
             });
         }
+
         return new Empty();
     }
-    
+
     private TokenBucket SetTokenBucketConfig(TokenBucket bucket, long capacity, long rate, bool isEnable)
     {
+        if (bucket.LastUpdatedTime != null)
+        {
+            var timeDiff = (Context.CurrentBlockTime - bucket.LastUpdatedTime).Seconds;
+            if (timeDiff != 0)
+            {
+                bucket.CurrentTokenAmount = CalculateRefill(bucket.TokenCapacity, bucket.CurrentTokenAmount, timeDiff,
+                    bucket.Rate);
+            }
+
+            bucket.CurrentTokenAmount = Math.Min(capacity, bucket.CurrentTokenAmount);
+        }
+        else
+        {
+            bucket.CurrentTokenAmount = capacity;
+        }
+
         bucket.TokenCapacity = capacity;
-        bucket.CurrentTokenAmount = capacity;
         bucket.Rate = rate;
         bucket.IsEnable = isEnable;
         bucket.LastUpdatedTime = Context.CurrentBlockTime;
@@ -138,12 +197,13 @@ public partial class BridgeContract
 
     public override TokenBucket GetCurrentReceiptTokenBucketState(GetCurrentReceiptTokenBucketStateInput input)
     {
-        var tokenBucket = State.ReceiptTokenBucketInfo[input.Symbol][input.Chain];
+        var tokenBucket = State.ReceiptTokenBucketInfo[input.Symbol][input.TargetChain];
         if (tokenBucket != null)
         {
             tokenBucket.CurrentTokenAmount = CalculateRefill(tokenBucket.TokenCapacity, tokenBucket.CurrentTokenAmount,
                 Context.CurrentBlockTime.Seconds.Sub(tokenBucket.LastUpdatedTime.Seconds), tokenBucket.Rate);
         }
+
         return tokenBucket ?? new TokenBucket();
     }
 
@@ -155,12 +215,13 @@ public partial class BridgeContract
             tokenBucket.CurrentTokenAmount = CalculateRefill(tokenBucket.TokenCapacity, tokenBucket.CurrentTokenAmount,
                 Context.CurrentBlockTime.Seconds.Sub(tokenBucket.LastUpdatedTime.Seconds), tokenBucket.Rate);
         }
+
         return tokenBucket ?? new TokenBucket();
     }
 
     public override Int64Value GetReceiptMinWaitTimeInSeconds(GetReceiptMinWaitTimeInSecondsInput input)
     {
-        var bucket = State.ReceiptTokenBucketInfo[input.Symbol][input.Chain];
+        var bucket = State.ReceiptTokenBucketInfo[input.Symbol][input.TargetChain];
         return new Int64Value
         {
             Value = GetMinWaitTimeInSeconds(bucket, input.TokenAmount)
@@ -176,7 +237,7 @@ public partial class BridgeContract
         };
     }
 
-    private long GetMinWaitTimeInSeconds(TokenBucket bucket,long amount)
+    private long GetMinWaitTimeInSeconds(TokenBucket bucket, long amount)
     {
         long minWaitInSeconds = 0;
         if (bucket != null && amount > bucket.CurrentTokenAmount)
