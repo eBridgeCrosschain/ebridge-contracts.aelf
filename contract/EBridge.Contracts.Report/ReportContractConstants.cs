d173266dfd966e45efd515b9b0e8216c7f485338
sherry <sherry@ebridge.exchange>
2024-05-10 00:31:49 +0800

    feat:support evm auto receive.

--- contract/EBridge.Contracts.Report/ReportContractConstants.cs
+++ contract/EBridge.Contracts.Report/ReportContractConstants.cs
@@ -5,5 +5,7 @@ namespace EBridge.Contracts.Report
         private const long MinimumAmercementAmount = 100_00000000;
         private const int MaximumOffChainQueryInfoCount = 1023;
         private const long DefaultApplyObserverFee = 200_00000000;
+        private const string DefaultReceiptInfoKey = "ReceiptInfoKey";
+
     }
 }
